e5ba579b253cca3be8d860e9571e7d106283fe14
Weipeng cao <wwcao@colorado.edu>
2017-03-06 12:44:33 -0700

    pipeline operations

---
+++ include/pipeline.h
@@ -0,0 +1,32 @@
+#include "static.h"
+#ifndef __PIPELINE_HEADER__
+#define __PIPLINE_HEADER__
+
+#define IS_PIPELINE         0
+#define RUNTOEND            0
+
+void IF() {
+
+}
+
+void ID() {
+
+}
+
+void EX() {
+
+}
+
+void MEM() {
+
+}
+
+void WB() {
+
+}
+
+void START() {
+
+}
+
+#endif
