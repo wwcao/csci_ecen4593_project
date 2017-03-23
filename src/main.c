79640f59a1875cdd862da97a9fd82e09bb101aa8
Weipeng cao <wwcao@colorado.edu>
2017-03-23 16:45:06 -0600

    minor changes

--- src/main.c
+++ src/main.c
@@ -6,22 +6,8 @@
 
 unsigned int memory[MEMORY_SIZE];
 unsigned int register_file[REGISTER_NUM];
-/* used pc instead
-unsigned int instruction;
-
-int main() {
-    readInstruction();
-    instruction = 0;
-    while(instruction < 10){
-	
-
-	start();
-        instruction+=1;
-    }
-
-    return 0;
-*/
 unsigned int PC;
+
 int main() {
 	unsigned int i;
 	unsigned int counter;
@@ -30,7 +16,7 @@ int main() {
 	i = readInstruction();
 	while(1){
 			start();
-			//printRegisters();
+			printRegisters();
 			if(((PC+4)>>2) > i) {
 				printf("Done\n");
 				break;
