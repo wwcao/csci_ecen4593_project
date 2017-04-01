f0d04dcc430f72cb562913c8084d214c7205d7bf
Weipeng cao <wwcao@colorado.edu>
2017-03-31 19:59:15 -0600

    Added cache files

--- src/cache.c
+++ src/cache.c
@@ -1,1 +1,16 @@
+#include "static.h"
 
+#ifndef __CACHE_HEADER__
+#define __CACHE_HEADER__
+
+#define L1_SIZE
+
+typedef struct {
+  bool valid;
+  unsigned int tag;
+  unsigned int value;
+} Cache;
+
+Cache cacheL1[];
+
+#endif // __CACHE_HEADER__
