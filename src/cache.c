598a0fb500b1507325a2081cf024eb00b2273527
Theerarun Tubnonghee <thtu5192@colorado.edu>
2017-04-10 13:17:52 -0600

    Update cache.c
    modify idea of pointer to new function

--- src/cache.c
+++ src/cache.c
@@ -1,4 +1,5 @@
 #include "static.h"
+#include <math.h>
 
 #ifndef __CACHE_HEADER__
 #define __CACHE_HEADER__
@@ -13,48 +14,101 @@
 
 typedef struct {
   bool valid;
-  unsigned int tag;
+  char tag;
   unsigned int value;
 } Cache;
+
+unsigned int penalty = 0;
 
 Cache cacheL1[];
 #endif // __CACHE_HEADER__
 
-bool readAddr(Cache cache[], unsigned int addr_index){
-	unsigned int tag = get_tag(addr_index);
-	unsigned int index = get_index(addr_index);
-	int i;
-	for(i=0; i<L1_BLOCKSIZE<i++){
-		if( i==index && tag == cache[i].tag && cache[i].valid == true ){
-			return true
-		}
+bool readAddr(Cache cache[], char* addr_index){
+	char* tag = get_tag(addr_index);
+	int index = get_index(addr_index);
+
+	if( tag == cache[index].tag && cache[index].valid == true ){
+		return true;
 	}
-
-	return false
+	updateAddr(cache[],tag,index,addr_index);
+	return false;
 }
 
-int get_tag(unsigned int addr_index){
- unsigned int tagBit= 32 - (L1_BLOCKSIZE>>2) - 2;
- 
+void updateAddr(Cache cache[],char* tag, int index, char* addr_index){
+	
+	cache[index].valid = true;
+	cache[index].tag = tag;
+	cache[index].value = btoi(addr_index);
+
 }
 
-bool is_hit(Cache cache[], unsigned int *addr_index){
-
- unsigned int index;
- for(i=0; i< L1_SIZE; i++){
-  if(cache[i].value == *addr_index){
-   
-    return true;
-  }
- }
-
- return false;
+int btoi(char* binary){
+	int i;
+   	int result = 0;
+   	int power = 0;
+   	for (i = strlen(binary) - 1; i >= 0; i--) {
+    	    int added = (binary[i] - '0') * (int)pow(2, power);
+    	   	result += added;
+    	   	power++;
+   	}
+   	return result;
 }
 
-int set_penalty(bool isHit){
+char* htob( char* address){
+    int i;
+   	char *binary = malloc(sizeof(char) * 33);
+    for(i = 2; i < strlen(address); i++) {
+        switch(address[i]) {
+		    case '0': strcat(bin_add,"0000"); break;
+            case '1': strcat(bin_add,"0001"); break;
+            case '2': strcat(bin_add,"0010"); break;
+            case '3': strcat(bin_add,"0011"); break;
+            case '4': strcat(bin_add,"0100"); break;
+            case '5': strcat(bin_add,"0101"); break;
+            case '6': strcat(bin_add,"0110"); break;
+            case '7': strcat(bin_add,"0111"); break;
+            case '8': strcat(bin_add,"1000"); break;
+            case '9': strcat(bin_add,"1001"); break;
+            case 'a': strcat(bin_add,"1010"); break;
+            case 'b': strcat(bin_add,"1011"); break;
+            case 'c': strcat(bin_add,"1100"); break;
+            case 'd': strcat(bin_add,"1101"); break;
+            case 'e': strcat(bin_add,"1110"); break;
+            case 'f': strcat(bin_add,"1111"); break;
+        	}
+    	}
+	strcat(binary,"\0");
+    return binary;	
+}
 
- if(isHit) return 0;
- 
- return MISS_PENALTY;
- 
+char* get_tag(char* addr_index){
+	unsigned int tagBit= 32 - (L1_BLOCKSIZE>>2) - 2;
+   	char *binary = malloc(sizeof(char) * 33);
+   	binary = htob(addr_index);
+   	char *tag = malloc(sizeof(char)*tagBit);
+   	tag = binary[2:tagBit+2];
+
+
+	return tag;
+
+}
+
+int get_index(unsigned int addr_index){
+	unsigned int indexBit= L1_BLOCKSIZE>>2;
+ 	char *binary = malloc(sizeof(char) * 33);
+   	binary = htob(addr_index);
+   	char *indexBin = malloc(sizeof(char)*indexBit);
+   	indexBin = binary[31-indexBit:31];
+   	int index = btoi(indexBin);
+
+	return index;
+}
+
+bool isHit(Cache cache[], char* addr_index){
+	return readAddr(cache[],addr_index);
+}
+
+int set_penalty(bool is_hit){
+	if(is_hit) return 0;
+	return MISS_PENALTY;
 }
