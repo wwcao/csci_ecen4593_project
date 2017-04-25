33c92d9e43fe179462b5068dfbac4e86a44c5915
wwcao <wwcao@colorado.edu>
2017-04-25 00:55:47 -0600

    Minor change

--- src/units.c
+++ src/units.c
@@ -14,7 +14,7 @@ void init_units() {
 void init_registers() {
 
   int i;
-  for(i = 0; i < 32; i++) {
+  for(i = 0; i < 32; i+=4) {
     register_file[i] = 0;
     register_file[i+1] = 0;
     register_file[i+2] = 0;
